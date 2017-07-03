0862f45fc167acb15eff2231e9491ea42f37919d
willsmorgan <wsmorgan@asu.edu>
2017-07-03 11:03:51 -0700

    Update 1 - 7.3.2017
    This is the first dump of the work that I've done and includes Labs 2-5

---
+++ Code/Chapter 3 - Linear Regression.R
@@ -0,0 +1,145 @@
+#This script corresponds with Chapter 3 of Introduction to Statistical Learning
+#3.6 Lab - Linear Regression
+
+library("MASS") #collection of data sets and functions
+library("ISLR") #data sets associated with the text
+library("car") #vif()
+###### 3.6.2 - Simple Linear Regression ######
+#Objective - use Boston data set to predict median house value in neighborhoods around Boston
+
+#Load our data set, and take a quick glance and it's structure
+fix(Boston)
+names(Boston)
+
+#Start with a basic regression using the lm(y ~ x, data) function
+lm.fit <- lm(medv ~ lstat, data = Boston)
+
+#Alternatively, you can attach the data set so that R knows to look for medv and lstat in the lm() function
+attach(Boston)
+lm.fit <- lm(medv ~ lstat)
+
+#To get the detailed results of the regression, we use summary() on the lm object
+summary(lm.fit)
+
+#The lm object contains quite a bit of information, so we use names() to parse it out and find what we want
+names(lm.fit)
+
+#An obvious result of interest is the coefficient values; we can call on them by name (lm.fit$coefficients) or use coef()
+coef(lm.fit)
+
+#For confidence intervals around these estimates, use the confint() function
+confint(lm.fit)
+
+#predict() is used to produce confidence intervals and prediction intervals for the prediction of medv for a given value of lstat
+predict(lm.fit, data.frame(lstat=c(5,10,15)), interval = "confidence")
+
+#Now let's visualize this least squares regression line
+plot(lstat, medv)
+abline(lm.fit)
+
+#abline() allows us to draw any line, so play around with the options lwd (length), col (color), and pch (plotting symbol)
+abline(lm.fit, lwd = 3)
+abline(lm.fit, lwd=3, col= "red")
+plot(lstat, medv, col="red")
+plot(lstat, medv, pch=20)
+plot(lstat, medv, pch="+")
+plot(1:20, 1:20, pch=1:20)
+
+#The lm() function automatically produces diagnostic plots, which we can see using plot()
+plot(lm.fit)
+
+#Instead of viewing the 4 graphs individually, we can tell R to split the display into 4 plots
+par(mfrow=c(2,2))
+plot(lm.fit)
+
+#Residuals can be plotted individually with residuals() or rstudent()
+plot(predict(lm.fit), residuals(lm.fit)) 
+plot(predict(lm.fit), rstudent(lm.fit))
+
+#Leverage statistics can be computed for our predictors with hatvalues(); to find the observation with the largest leverage which.max() is used
+plot(hatvalues(lm.fit))
+which.max(hatvalues(lm.fit))
+
+###### End of 3.6.2 ######
+
+###### 3.6.3 - Multiple Linear Regression ######
+#Objective - Extend the current example using multiple predictors
+
+#In order to add more predictors to our model, we use "+" in the lm syntax
+lm.fit <- lm(medv ~ lstat + age, data=Boston)
+summary(lm.fit)
+
+#It is tedious to type out every one of our predictors, so we can use "." on the right hand side of the regression equation
+lm.fit <- lm(medv ~ ., data=Boston)
+summary(lm.fit)
+
+#Check out the R^2 and Residual Squared Error of this model
+summary(lm.fit)$r.sq
+summary(lm.fit)$sigma
+
+#Variance Inflation Factor (vif) is found with vif() in the "car" package
+vif(lm.fit)
+
+#If we want to exclude one or more of the predictors from our model, use -varname on the right hand side of the equation
+lm.fit1 <- lm(medv ~ . -age, data=Boston)
+summary(lm.fit1)
+lm.fit1 <- update(lm.fit, ~.-age)
+
+#Finally, we can include interaction terms in the model by multiplying the two variables of interest within the lm() function
+summary(lm(medv ~ lstat*age, data=Boston))
+
+###### End of 3.6.3 ######
+
+#Note - 3.6.4 was included in the previous section because it was so short
+
+###### 3.6.5 Non-linear Transformations of the Predictors ######
+
+#lm() can accomodate for transformations of the predicors using I(varname)
+lm.fit2 <- lm(medv ~ lstat + I(lstat^2))
+summary(lm.fit2)
+
+#To quantify how much better the quadratic terms fits the model, use anova()
+lm.fit <- lm(medv ~ lstat)
+anova(lm.fit, lm.fit2)
+
+#For more evidence, let's visualize the residuals
+par(mfrow=c(2,2))
+plot(lm.fit2)
+
+#We can include higher order polynomials by using poly(var, degree) within the lm function
+lm.fit5 <- lm(medv ~ poly(lstat, 5))
+summary(lm.fit5)
+
+#Last but not least, check out a log transformation of our predictor "rm"
+summary(lm(medv ~ log(rm), data=Boston))
+
+###### End of 3.6.5 ######
+
+###### 3.6.6 - Qualitative Predictors ######
+#Objective - use the Carseats data set in the ISLR library to predict sales based on a number of predictors
+
+fix(Carseats)
+names(Carseats)
+
+#Fortunately, R is able to recognize categorical variables and will automatically generate dummies when we run a regression
+lm.fit <- lm(Sales ~ . + Income:Advertising + Price:Age, data=Carseats) #using ":" is another way to include interactions
+summary(lm.fit)
+
+#The contrasts() function returns the coding that R uses for the dummy variables
+attach(Carseats)
+contrasts(ShelveLoc)
+
+###### End of 3.6.6 ######
+
+###### 3.6.7 - Writing Functions (optional, but very useful) ######
+#Objective - write a custom function to load the libraries we use over and over
+
+loadlibraries <- function() {
+  library("ISLR")
+  library("MASS")
+  print("The libraries have been loaded")
+}
+
+loadlibraries()
+
+###### End of 3.6.7 ######
---
+++ Code/Chapter 5 - Resampling Methods.R
@@ -0,0 +1,138 @@
+#This script corresponds with Chapter 5 of Introduction to Statistical Learning
+#5.3 Lab - Cross-Validation and the Bootstrap
+
+set.seed(1)
+library("ISLR") #Auto, Portfolio data sets
+library("boot") #cv.glm()
+###### 5.3.1 - The Validation Set Approach ######
+#Objective - estimate test error rates that result from fitting various linear models on the Auto data set
+
+#We randomly generate a vector of integers to tell us how to divide up our data
+attach(Auto)
+train <- sample(392,196)
+
+#Fit the model with the training set, and find the calculate the mean squared erroro
+lm.fit <- lm(mpg ~ horsepower, data = Auto, subset = train)
+mean((mpg - predict(lm.fit, Auto))[-train]^2)
+
+#Refit the model using higher order terms
+lm.fit2 <- lm(mpg ~ poly(horsepower,2), data = Auto, subset = train)
+mean((mpg - predict(lm.fit2, Auto))[-train]^2)
+
+lm.fit3 <- lm(mpg ~ poly(horsepower,3), data = Auto, subset = train)
+mean((mpg - predict(lm.fit3, Auto))[-train]^2)
+
+#If we want to find a different training set, simply change the seed and repeat this process
+set.seed(2)
+train <- sample(392,196)
+
+lm.fit <- lm(mpg ~ horsepower, data = Auto, subset = train)
+mean((mpg - predict(lm.fit, Auto))[-train]^2)
+
+lm.fit2 <- lm(mpg ~ poly(horsepower,2), data = Auto, subset = train)
+mean((mpg - predict(lm.fit2, Auto))[-train]^2)
+
+lm.fit3 <- lm(mpg ~ poly(horsepower,3), data = Auto, subset = train)
+mean((mpg - predict(lm.fit3, Auto))[-train]^2)
+
+### Pause for Analysis ###
+#The error terms are consistent across training sets and fits, we can also see that the quadratic term results in the least MSE for each sample
+
+###### End of 5.3.1 ######
+
+###### 5.3.2 - Leave-One-Out Cross-Validation ######
+#Objective - Conduct LOOCV on the Auto data and observe error rates 
+
+#The LOOCV estimate can be automatically estimated using the cv.glm() function, which is found in the "boot" library
+glm.fit <- glm(mpg ~ horsepower, data = Auto)
+cv.err <- cv.glm(Auto, glm.fit)
+cv.err$delta
+
+### Pause for analysis ###
+#cv.glm() provides us a list with several components:
+  #The delta vector contains the CV results - the average of the n test error rates
+
+#Let's repeat this process for increasingly large polynomial terms and see how $delta changes
+cv.error <- rep(0,5)
+for (i in 1:5){
+  glm.fit <- glm(mpg~poly(horsepower, i), data = Auto)
+  cv.error[i] <- cv.glm(Auto, glm.fit)$delta[1]
+}
+
+cv.error
+
+### Coding Tip ###
+#Notice how we create the cv.error vector to accomodate the loop:
+  #Before the loop begins, we create a empty vector that has the same length as the number of iterations of the loop (5)
+  #With each iteration of the loop, a new model is created and a new MSE estimate is found
+  #The indexing variable, i, tells us which iteration we are on and allows us to throw the newest estimate into the correct position in the cv.error vector
+
+### Pause for analysis ###
+#We can see that the average MSE drops off when we change to a quadratic term, the it doesn't really improve with higher degrees
+
+###### End of 5.3.2 #######
+
+###### 5.3.3 k-Fold Cross-Validation ######
+#Objective - Use k-fold CV to generate estimates for the MSE
+
+#The cv.glm() function we used earlier allows for k-fold CV, so we can repeat pretty much the exact same process
+set.seed(17)
+cv.error.10 <- rep(0,10)
+for (i in 1:10){
+  glm.fit <- glm(mpg~poly(horsepower, i), data = Auto)
+  cv.error.10[i] <- cv.glm(Auto, glm.fit, K = 10)$delta[1]
+}
+cv.error.10
+
+###### End of 5.3.3 ######
+
+###### 5.3.4 - The Bootstrap ######
+#Objective - Illustrate a use of bootstrapping with the Portfolio and Auto data sets (ISLR)
+
+#Bootstrapping in R requires the computation of a statistic of interest
+  #In this example, we write a function alpha.fn() to find the proportion of our money to invest in X (alpha) that minimizes variance below:
+    #Var(alpha*X + (1-alpha)Y)
+alpha.fn <- function(data, index){
+  X <- data$X[index]
+  Y <- data$Y[index]
+  return((var(Y)-cov(X,Y))/(var(X)+var(Y)-2*cov(X,Y)))
+}
+
+#Now, we randomly select 100 observations (with replacement) from the portfolio to find an bootstrapped estimate of alpha
+set.seed(1)
+alpha.fn(Portfolio, sample(100,100, replace=T))
+
+#To do conduct a bootstrap analysis we'd want to repeat this process, record the estimates for alpha, and find its standard deviation
+  #Luckily, boot() does this for us
+boot(Portfolio, alpha.fn, R=1000)
+
+#Bootstrapping can be used to assess the variablility of coefficient estimates and predictions from a specific learning method:
+  #Here, we use it to assess the variablility of the estimates of the coefficients in a linear regression model that predicts mpg using horsepower
+  #We first define a function that takes in the Auto data, runs a regression and returns the estimated coefficients
+  #Then, we can randomly generate bootstrapped samples and iterate as many times as we'd like
+boot.fn <- function(data, index){
+  return(coef(lm(mpg~horsepower, data=data, subset=index)))
+}
+
+set.seed(1)
+boot.fn(Auto, 1:392) #Return the coefficients of the model that uses all observations
+boot.fn(Auto, sample(392,392,replace=T)) #Return the coefficients of the model that samples 392 observations (w/replacement) from the original sample
+
+boot(Auto, boot.fn, 1000)
+summary(lm(mpg~horsepower, data=Auto))$coef #show this output so we can analyse differences in SEs 
+
+### Pause for Analysis ###
+#From this output we can see that the standard error for estimated intercept and slope terms are .8655 and .0076 respectively (in the bootstrap)
+#Furthermore, why might there be a difference in the bootstrapped SEs and the single linear model?
+  #It is because the estimate for variance is biased upwards
+
+#Let's repeat what we just did above using a quadratic term 
+boot.fn <- function(data, index){
+  return(coef(lm(mpg~horsepower + I(horsepower^2), data=data, subset=index)))
+}
+
+set.seed(1)
+boot(Auto, boot.fn, 1000)
+summary(lm(mpg~horsepower+I(horsepower^2), data=Auto))$coef
+
+###### End of 5.3.4 ######

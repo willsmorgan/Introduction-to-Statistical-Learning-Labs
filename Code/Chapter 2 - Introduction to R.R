75df2807516840d1686b85531885c0cb5c862f9c
willsmorgan <wsmorgan@asu.edu>
2017-07-03 11:03:51 -0700

    Update 1 - 7.3.2017
    This is the first dump of the work that I've done and includes Labs 2-5

---
+++ Code/Chapter 2 - Introduction to R.R
@@ -0,0 +1,150 @@
+#This script corresponds with Chapter 2 of Inroduction to Statistical Learning
+#2.3 Lab: Introduction to R
+library(ISLR)
+
+##### SECTION 1: Getting Started with R ######
+#We can create a vector x of any length by using the c() command
+x <- c(1,3,2,5)
+
+#Note that instead of using <- to create an object we can simply use = 
+x = c(1,6,2)
+y = c(1,4,3)
+
+#We can apply standard mathematical operations to numeric vectors of similar lengths:
+x+y
+x*y
+
+#To find the length of a vector, simply use the command length()
+length(x)
+length(y)
+
+#To see all of the objects in our environment, we can use the command ls()
+ls()
+
+#We can remove objects from our environment using rm()
+rm(x,y)
+
+#It is possible to remove all objects at once:
+rm(list=ls())
+
+#The matrix() function can be used to create a matrix of numeric values
+x <- matrix(data=c(1,2,3,4), nrow=2, ncol=2)
+x
+
+#The byrow option will fill the matrix horizontally with the data
+matrix(c(1,2,3,4),2,2, byrow=TRUE)
+
+#As before, standard operations can be applied to matrices
+x^2
+sqrt(x)
+
+#The rnorm() function generates a vector of random normal variables, and cor() finds the correlation between two variables
+x <- rnorm(50)
+y <- x + rnorm(50, mean=50, sd=1)
+cor(x,y)
+
+#The set.seed() function will allows us to reproduce our code more easily when generating random variables
+set.seed(1303)
+rnorm(50)
+
+#mean(), var(), and sd() are more mathematical functions that are self-explanatory
+set.seed(3)
+y <- rnorm(100)
+mean(y)
+var(y)
+sqrt(var(y))
+sd(y)
+
+##### END OF SECTION 1 ######
+
+##### SECTION 2: Graphics ######
+#The plot() function is the quickest way to plot data; plot(x,y) will give us a scatter plot between x and y
+x <- rnorm(100)
+y <- rnorm(100)
+plot(x,y)
+plot(x,y,xlab="this is the x-axis", ylab="this is the y-axis", main="x vs y")
+
+#To save the image of the plots we create, there are several options
+pdf("figure 1: basic ass scatterplot.pdf")
+plot(x,y,col="green")
+dev.off() #This function tells R that we are done creating the plot
+
+#seq(a,b,length=.) is a function that allows us to generate a sequence of numbers between a and b with a specified length
+x <- seq(1,10)
+x
+x <- 1:10 #a:b is shorthand for seq(1,10)
+x <- seq(-pi, pi, length=50)
+
+#Let's try to graph three-dimensional data using contour()
+y <- x 
+
+f <- outer(x,y,function(x,y) cos(y)/(1+x^2)) #We write a cosine function whose domain is x,y and range is f
+contour(x,y,f)
+contour(x,y,f,nlevels=45,add=T)
+
+fa=(f-t(f))/2 #Transpose our range matrix and transform it
+contour(x,y,fa,nleveles=15)
+
+#image() works in the same way, except that it adds color (much like a heatmap)
+image(x,y,fa)
+
+#persp() will give us a three-dimensional plot, with options theta and phi to rotate the viewing angles
+persp(x,y,fa)
+persp(x,y,fa, theta=30)
+persp(x,y,fa, theta=30, phi=20)
+persp(x,y,fa, theta=30, phi=40)
+persp(x,y,fa, theta=30, phi=70)
+
+##### END OF SECTION 2 #####
+
+##### SECTION 3: Loading and Indexing Data #####
+A <- matrix(1:16, 4,4)
+
+#Square brackets are used for indexing and selecting subsets of data
+  #The first number in the bracket refers to the row, and the second number refers to the column
+  #In order to grab an entire row or an entire column, use a comma in place of a number
+  #A negative sign indicates to R that everything but the listed numbers will be grabbed
+
+A[2,3]
+A[c(1,3),c(2,4)]
+A[1:3,2:4]
+A[,2]
+A[2,]
+A[-c(1,3),]
+
+#dim() outputs the number of rows followed by the number of columns
+dim(A)
+
+#We begin by loading the Auto dataset taken from the ISLR website; it is saved in csv format so we must use read.csv()
+auto <- read.csv("Data\Auto.csv")
+fix(auto) #quick visual inspection
+dim(auto) #get an idea of how large this dataset is
+names(auto) #check out the names of the variables in this data
+
+#Deal with missing observations by omitting them
+auto <- na.omit(auto)
+
+#Let's begin visualizing some of this data
+attach(auto) #attach() allows the user to call on the column names of auto directly
+plot(cylinders, mpg)
+
+cylinders <- as.factors(cylinders) #This variable is more appropriately interpreted as a categorical variable
+plot(cylinders, mpg) #Now that cylinders is a factor, plot() will output a boxplot
+plot(cylinders, mpg, col="red")
+plot(cylinders, mpg, col="red", varwidth=T)
+
+#hist() can be used to plot a histogram
+hist(mpg)
+hist(mpg, col=2) #note that col=2 is equivalent to col="red"
+hist(mpg, col=2, breaks=15)
+
+#pairs() creates a scatterplot matrix for every pair of variables in a given data set; this can also be for a subset of those variables
+pairs(auto)
+pairs(~ mpg + displacement+ horsepower + weight + acceleration, auto)
+
+#The identify() function lets you manually select points on a plot to identify
+plot(horsepower, mpg)
+identify(horsepower, mpg, name)
+
+#summary() gives a numerical summary of each variable in a data set
+summary(auto)
---
+++ Code/Chapter 4 - Logistic Regression, LDA, QDA, and KNN.R
@@ -0,0 +1,261 @@
+#This script corresponds with Chapter 4 of Introduction to Statistical Learning
+#4.6 Lab - Logistic Regression, LDA, QDA, and KNN
+
+library("MASS") #lda()
+library("ISLR") #location of Smarket data
+library("class") #knn()
+
+#Note - 4.6.1 is integrated into 4.6.2 because of length
+
+###### 4.6.2 - Logistic Regression ######
+#Objective - Fit a logistic regression to predict the direction of stock market data (Smarket)
+
+#Begin with a cursory inspection of the data structure
+colnames(Smarket) <- tolower(colnames(Smarket)) 
+attach(Smarket)
+
+### Writing Tip ###
+#Sometimes variable names are capitalized inconsistently and can be difficult to remember;
+#Simply change all the names of the variables (column names) to lowercase so you can be sure how they are written
+
+names(Smarket) #Lag1 - Lag5 indicate the percentage returns for each of the five previous trading days
+dim(Smarket)
+summary(Smarket)
+cor(Smarket[,-9]) #The 9th column variable, Direction, is qualitative so it should be excluded from our correlation matrix
+
+plot(volume)
+
+#We now fit a logit model in order to predict Direction using Lag1-Lag5 and Volume with the glm() function
+glm.fit <- glm(direction ~ lag1 + lag2 + lag3 + lag4 + lag5 + volume, 
+               data=Smarket, family="binomial") #The family="binomial" argument tells R we are running a logistic regression
+summary(glm.fit)
+summary(glm.fit)$coef
+
+### Writing Tip ###
+#When a function has many arguments, the line of code can get annoyingly long to read. To make it easier, 
+  #start a new indented line after an argument (just like this :P)
+
+### Pause for Analysis ###
+#Take a look at the coefficient for lag1 - it is negative, which implies that positive returns yesterday mean less chance of stock increase today
+  #However, the p-value of lag1 (0.15) is kind of large, so there's not clear evidence of this relation being true
+
+#Because our output is in log-odds, we need to use the option type="response" when making predictions if we want P(Y=1|X)
+glm.probs <- predict(glm.fit, type="response")
+glm.probs[1:10]
+
+#To make sure that these probabilities are for the market going Up, check how R codes direction - we need to know that Y=1 implies up
+contrasts(direction)
+
+#Instead of looking at these probabilties one by one, we can set a threshold to tell us what our observations predict about the market
+glm.pred <- rep("Down", 1250)
+glm.pred[glm.probs>.5] = "Up"
+
+#Now that we've got predicted values and true values of direction, we can create a confusion matrix to assess accuracy
+table(glm.pred, direction)
+mean(glm.pred==direction)
+
+### Pause for Analysis ###
+#The output of the mean() function tells us the fraction of days that our model predicted correctly - 52.16% of the time
+  #That means that our model has a 47.84% training error rate - hardly better than flipping a coin
+  #Furthermore, the training error rate generally underestimates the test error rate, so the actual accuracy may be worse
+
+#The training data for the model is the entire data set, leaving nothing for testing. Let's split up the data to create a test set
+train <- (year < 2005) #Create training set indicator for the years 2001-2004
+Smarket.2005 <- Smarket[!train, ] #Subset the original data to keep data with train == 0
+direction.2005 <- Smarket.2005[,"direction"] #true values of the test set
+
+### Coding Tip ###
+#Instead of splitting the original data (Smarket) into two new sets, training and testing, we simply use a new indicator variable for training obs.
+#This is useful when working with extremely large data sets and will keep RAM usage down
+
+#Fit a new logistic regression using only the subset of observations for which train == 1, and then find how that model predicts the test set
+glm.fit <- glm(direction ~ lag1 + lag2 + lag3 + lag4 + lag5 + volume, data = Smarket,
+               family = "binomial", subset = train)
+glm.probs <- predict(glm.fit, Smarket.2005, type = "response")
+
+#Create Yes/No predictions just as we did with the previous model
+glm.pred = rep("Down", length(glm.probs))
+glm.pred[glm.probs>.5] = "Up"
+
+### Coding Tip ###
+#For better reproducibility of your code, it's usually a good idea to refrain from using explicit numbers, like we did in line 48 with glm.pred
+  #Instead, try to find ways to generalize what you're trying to do so that it can be done again without many edits to the original code
+  #For instance, when we create the "Down" vector,  instead of using the number 252 in the second argument we just say length(glm.probs)
+  #Both are equivalent lines of code, but the second allows us to easily change the size of the test set and not worry about running into errors
+
+table(glm.pred, direction.2005)
+mean(glm.pred == direction.2005)
+
+### Pause for Analysis ###
+#The results aren't that great - the test error rate is 52%
+#We noticed earlier that the p-values were pretty lackluster, so it might be worth investigating a similar model that excludes the terms with the highest p-values
+
+#Let's fit a new model, only using lag1 and lag2 as predictors
+glm.fit = glm(direction ~ lag1 + lag2, data = Smarket, family = "binomial", subset = train)
+glm.probs = predict(glm.fit, Smarket.2005, type = "response")
+
+glm.pred <- rep("Down", length(glm.probs))
+glm.pred[glm.probs>.5] = "Up"
+
+table(glm.pred, direction.2005)
+mean(glm.pred == direction.2005)
+
+### Pause for Analysis ###
+#Results are a little better - 55.95% of our observations were predicted correctly by the model
+#Further still, it has a 58% rate of correctly predicting an upward movement in the market (True Positives / (True Positives + False Positives))
+
+### End of 4.6.2 ###
+
+### 4.6.3 - Linear Discriminant Analysis ###
+#Objective - Perform LDA on the Smarket data to predict direction
+
+#We use the lda() function on the training data that we previously created; notice that the syntax is identical to lm() and glm()
+lda.fit <- lda(direction ~ lag1 + lag2, data = Smarket, subset = train)
+lda.fit
+
+### Pause for Analysis ###
+#The output first tells us that 49.19% of our training data corresponds to days where the market went down, and 50.8% went up
+#The group means is a cross-tabulation of the means of the predictors for each level of the dependent variable, direction
+#Lastly, the coefficients of linear discriminants gives us the linear combination of lag1 and lag2 that create the decision boundary
+  #If the expression -.64*lag1 - .5135*lag2 is large, the LDA classifier will predict a market increase
+  #Likewise, if the above expression is small, the LDA classifier will predict a market decrease
+
+#The plot() function produces plots of the linear discriminants, obtained by plugging in values of lag1 and lag2 for each of the training obs.
+plot(lda.fit)
+
+### Pause for Analysis ###
+#Observe the distributions of the plots for Up and Down - it is centered mostly around 0
+  #What does this tell us? 
+    #Because there are very few extreme values in these distributions, the model has a tough time predicting market movement in either class
+
+#Now we make some predictions, and see how our results compare to those of the logistic regression
+lda.pred = predict(lda.fit, Smarket.2005)
+names(lda.pred) 
+  #Notice the output of lda.pred:
+    #"class" tells us the prediction of the LDA
+    #"posterior" is a matrix of probailities of the observations belonging to that class
+    #"x" contains the linear discrminants
+
+lda.class = lda.pred$class
+table(lda.class, direction.2005)
+mean(lda.class==direction.2005) #The mean is pretty much identical to that of the logistic regression!
+
+### End of 4.6.3 ###
+
+### 4.6.4 - Quadratic Discriminant Analysis ###
+#Objective - Fit a QDA model to the Smarket data and observe any difference from the previous methods
+
+#The syntax for the qda() function is identical to lda(), so we can quickly replicate what we just did
+qda.fit <- qda(direction ~ lag1 + lag2, data = Smarket, subset = train)
+qda.fit # Notice that the qda() function does not report the coefficients of the linear discriminants
+
+#Make the same usual predictions
+qda.class <- predict(qda.fit, Smarket.2005)$class
+table(qda.class, direction.2005)
+mean(qda.class==direction.2005) #Our mean has increased to about 60%, which suggests the quadratic form assumed by QDA better fits the relationship
+
+### End of 4.6.4 ###
+
+### 4.6.5 K-Nearest Neighbors ###
+#Objective - perform KNN using the knn() function, which is part of the class library
+
+#The syntax for knn() is different from the commands that we have used in the past;
+  #knn(train, test, cl, k)
+    #train - the matrix or data frame of training set cases
+    #test - the matrix or data frame of test set cases
+    #cl - a vector containing the class labels for the training observations
+    #k - the number of neighbors considered
+
+library("class")
+
+#Create the training and testing sets
+train.X <- cbind(lag1, lag2)[train,]
+test.X <- cbind(lag1, lag2)[!train,]
+train.direction <- direction[train]
+
+#Now use knn() to predict the market's movement  for the dates in 2005 (test set)
+set.seed(1)
+knn.pred <- knn(train.X, test.X, train.direction, k=1)
+table(knn.pred, direction.2005)
+mean(knn.pred==direction.2005)
+
+#We only used one nearest neighbor, so obviously the predictive power isn't great. Let's try it with k=3
+knn.pred <- knn(train.X, test.X, train.direction, k=3)
+table(knn.pred, direction.2005)
+mean(knn.pred==direction.2005)
+
+###### End of 4.6.5 ######
+
+###### 4.6.6 - An Application to Caravan Insurance Data ######
+#Objective - Use KNN to make predictions as to whether or not car insurance will be bought
+  #We will be using the Caravan data set found in the ISLR library
+
+#Load the data, attach it, and inspect
+dim(Caravan)
+colnames(Caravan) <- tolower(colnames(Caravan))
+attach(Caravan)
+sapply(Caravan, class) #check the class of each variable to see which are categorical and which are continuous
+
+summary(purchase)
+
+### Pause for quick note ###
+#The KNN classifier relies on identifying observations that are near one another, so the scale of the predictors is very important
+  #Any variables that are on a large scale will have a much larger effect on the distance between the observations
+    #ex: a difference of $1000 in salary has a much larger effect on the classifier than say a difference in 50 years of age
+  #As a result, classifiers tend to overstate the importance of variables with larger scales and understate smaller ones
+
+#All variables excluding purchase are numeric variables, so when we standardize the d.f. we ignore Purchase
+standardized.x <- scale(Caravan[,-86])
+var(Caravan[,1])
+var(Caravan[,2])
+
+var(standardized.x[,1])
+var(standardized.x[,2])
+
+#Now we split the observations into a test set, containing the first 1000 obs, and a training set, containing the remaining ones
+test <- 1:1000
+train.x <- standardized.x[-test,]
+test.x <- standardized.x[test,]
+
+train.y <- purchase[-test]
+test.y <- purchase[test]
+
+set.seed(1)
+knn.pred <- knn(train.x, test.x, train.y, k=1)
+mean(test.y!= knn.pred)
+mean(test.y!="No")
+
+### Pause for analysis ###
+#It seems like we've got a solid classifier - the test error rate is only 11.8%!
+#However, only 6% of people actually bought insurance, so we could have an error rate down to 6% if we changed the classifier to always predict No
+
+table(knn.pred, test.y)
+mean(knn.pred == test.y)
+
+#Let's repeat the process using more neighbors to see if we get a more accurate classifier
+knn.pred <- knn(train.x, test.x, train.y, k=3)
+table(knn.pred, test.y)
+mean(knn.pred == test.y)
+
+knn.pred <- knn(train.x, test.x, train.y, k=5)
+table(knn.pred, test.y)
+mean(knn.pred == test.y)
+
+#Now that we have our knn predictions, let's compare it to a logistic regression model using .5 as a cutoff
+glm.fit <- glm(purchase ~., data = Caravan, family = binomial, subset = -test)
+
+glm.probs <- predict(glm.fit, Caravan[test,], type = "response")
+glm.pred <- rep("No", 1000)
+glm.pred[glm.probs>.5] <- "Yes"
+
+table(glm.pred, test.y)
+mean(glm.pred == test.y)
+
+#We actually did a terrible job predicting people buying the insurance, they're all wrong! Change to cutoff probability to see if we get better accuracy
+glm.pred <- rep("No", 1000)
+glm.pred[glm.probs>.25] <- "Yes"
+
+table(glm.pred, test.y)
+mean(glm.pred == test.y)
+
+###### End of 4.6.6 ######
